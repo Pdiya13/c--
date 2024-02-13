e5ddedd1c7afe21e50d5689384baea2f35e61e0e
Pdiya13 <pdiya2945@gmail.com>
2024-02-13 10:02:46 +0530

    add files

--- prac.cpp
+++ prac.cpp
@@ -1,26 +1,28 @@
 #include<iostream>
  int x = 1;
+ 
  namespace nspace
  {
  int x = 2;
  int y = 3;
  int z = 4;
  }
+
  int main()
  {
  int y = 5;
- using namespace nspace;
+  using namespace nspace;
  //std::cout << " " << x;
- std::cout << " " << ::x;
- std::cout << " " << y;
- //std::cout << " " << ::y;
- std::cout << " " << z;
- //std::cout << " " << ::z;
+ std::cout << " " << ::x; //1
+ std::cout << " " << y;  
+ //std::cout << " " << ::y;  5
+ std::cout << " " << z;  //4
+ //std::cout << " " << ::z; 4
  void fun();
  fun();
  return 0;
  }
  void fun()
  {
- std::cout << " " <<  x;
+    std::cout << " " <<  x;
  }
